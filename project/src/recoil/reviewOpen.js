2c3da93a99b2e7114c2c1f995e50a6caf16e15cf
hiheeen <gmldms0338@naver.com>
2023-10-23 18:58:26 +0900

    refactor : 필요없는 코드와 파일 제거

--- project/src/recoil/reviewOpen.js
+++ project/src/recoil/reviewOpen.js
@@ -2,6 +2,5 @@ import { atom } from 'recoil';
 
 export const reviewOpenState = atom({
   key: 'reviewOpenState',
-  default: false, // 함수를 호출하여 초기 상태 설정
+  default: false,
 });
-// true : toggle-on , 나만 보고 싶은 기록
